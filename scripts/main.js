e1c09c1303a42bed1e79fcdd656503959d1cc18e
Łukasz Greszta <lukasz_greszta@o2.pl>
2019-12-11 13:37:12 +0100

    Use for and for..of

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,26 @@
-const image='kotek.png';
-console.log(image);
+const image1="https://picsum.photos/300/150";
+const image2="https://picsum.photos/300/150";
+const image3="https://picsum.photos/300/150";
+
+const images=[image1, image2, image3];
+
+//images.push("https://picsum.photos/500/300/150");
+
+//images.push(image1, image2, image3);
+
+
+// Pętla: for..of
+for (const image of images){
+    console.log("To jest obrazek " + image);
+    console.log("TEST");
+}
+console.log("Koniec pętli nr1");
+
+//Pętla: for
+//i++ => i=i+1
+for (let i=0; i<images.length; i++){
+    const image=images[i];
+    console.log("To jest obrazek " + image);
+    console.log("TEST");
+}
+console.log("Koniec pętli nr2");
